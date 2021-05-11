1f1e540625c8ecda8d33e8971ee8c209c8bccd58
Monika <m.szymanska.macik@gmail.com>
2021-05-11 22:02:29 +0200

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
+import { promise } from 'selenium-webdriver';
 
 @Component({
   selector: 'pm-root',
-  template:
-  `
-  <nav class='navbar navbar-expand navbar-light bg-light'>
-    <a class='navbar-brand'>{{pageTitle}}</a>
-    <ul class='nav nav-pills'>
-      <li><a class='nav-link'[routerLink]="['/clients']">Client List</a></li>
-      <li><a class='nav-link'[routerLink]="['/form']">Form</a></li>
-    </ul>
-  </nav>
-  <div class='container'>
-    <router-outlet></router-outlet>
-  </div>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // `
+  // <nav class='navbar navbar-expand navbar-light bg-light'>
+  //   <a class='navbar-brand'>{{pageTitle}}</a>
+  //   <ul class='nav nav-pills'>
+  //     <li><a class='nav-link'[routerLink]="['/clients']">Client List</a></li>
+  //     <li><a class='nav-link'[routerLink]="['/form']">Form</a></li>
+  //   </ul>
+  // </nav>
+  // <div class='container'>
+  //   <router-outlet></router-outlet>
+  // </div>
+  // `
 })
 
 export class AppComponent {
   pageTitle :  string = 'Hotel for Dogs';
   title: string = 'Hotel';
+  isAuthenticated = false;
+
+  async logout(): Promise<void>{
+
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,13 +3,19 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 const MaterialComponents = [
     MatSliderModule,
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
 ];
 
 @NgModule({
